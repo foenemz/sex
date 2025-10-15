1a43de0927540e361fb9c1e793520d770c493922
foenemz <semihmeze@gmail.com>
2025-10-15 06:08:33 +0100

    Add files via upload

---
+++ panorama_dll.cs
@@ -0,0 +1,14 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-10-15 04:19:54.959395600 UTC
+
+namespace CS2Dumper.Schemas {
+    // Module: panorama.dll
+    // Class count: 1
+    // Enum count: 0
+    public static class PanoramaDll {
+        // Parent: None
+        // Field count: 0
+        public static class ELayoutNodeType {
+        }
+    }
+}
